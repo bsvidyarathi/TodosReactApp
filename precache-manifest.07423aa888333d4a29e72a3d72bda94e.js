2bcb6e1afe8ee5b86292a080c12ebc24a54a103a
bsvidyarathi <bsvidyarathi@hotmail.com>
2020-09-21 10:36:50 +1000

    Updates

---
+++ precache-manifest.07423aa888333d4a29e72a3d72bda94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d24fb59485d1134d61a3daa73fe1b8",
+    "url": "/TodosReactApp/index.html"
+  },
+  {
+    "revision": "c6c1734ea71196d16541",
+    "url": "/TodosReactApp/static/css/main.79074318.chunk.css"
+  },
+  {
+    "revision": "735e3a42a953643d6403",
+    "url": "/TodosReactApp/static/js/2.12e6d84c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodosReactApp/static/js/2.12e6d84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c1734ea71196d16541",
+    "url": "/TodosReactApp/static/js/main.20b81847.chunk.js"
+  },
+  {
+    "revision": "9b53dd63e447d5d5dda2",
+    "url": "/TodosReactApp/static/js/runtime-main.ab973d8f.js"
+  }
+]);
